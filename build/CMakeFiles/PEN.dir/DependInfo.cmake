68a1bbf2c21cbd6afcad111a344e37c0ab4f5f88
Luis Manzanillas <manzanil@pclg-04.mppmu.mpg.de>
2020-04-08 11:48:49 +0200

    geant4 attenuation setup

---
+++ build/CMakeFiles/PEN.dir/DependInfo.cmake
@@ -0,0 +1,60 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/iwsatlas1/manzanil/Documents/LEGEND/PEN/AttenuationPenSetup/PEN.cc" "/home/iwsatlas1/manzanil/Documents/LEGEND/PEN/AttenuationPenSetup/build/CMakeFiles/PEN.dir/PEN.cc.o"
+  "/home/iwsatlas1/manzanil/Documents/LEGEND/PEN/AttenuationPenSetup/src/ActionInitialization.cc" "/home/iwsatlas1/manzanil/Documents/LEGEND/PEN/AttenuationPenSetup/build/CMakeFiles/PEN.dir/src/ActionInitialization.cc.o"
+  "/home/iwsatlas1/manzanil/Documents/LEGEND/PEN/AttenuationPenSetup/src/DetectorConstruction.cc" "/home/iwsatlas1/manzanil/Documents/LEGEND/PEN/AttenuationPenSetup/build/CMakeFiles/PEN.dir/src/DetectorConstruction.cc.o"
+  "/home/iwsatlas1/manzanil/Documents/LEGEND/PEN/AttenuationPenSetup/src/DetectorMessenger.cc" "/home/iwsatlas1/manzanil/Documents/LEGEND/PEN/AttenuationPenSetup/build/CMakeFiles/PEN.dir/src/DetectorMessenger.cc.o"
+  "/home/iwsatlas1/manzanil/Documents/LEGEND/PEN/AttenuationPenSetup/src/EventAction.cc" "/home/iwsatlas1/manzanil/Documents/LEGEND/PEN/AttenuationPenSetup/build/CMakeFiles/PEN.dir/src/EventAction.cc.o"
+  "/home/iwsatlas1/manzanil/Documents/LEGEND/PEN/AttenuationPenSetup/src/LightGuideConstruction.cc" "/home/iwsatlas1/manzanil/Documents/LEGEND/PEN/AttenuationPenSetup/build/CMakeFiles/PEN.dir/src/LightGuideConstruction.cc.o"
+  "/home/iwsatlas1/manzanil/Documents/LEGEND/PEN/AttenuationPenSetup/src/PenMaterials.cc" "/home/iwsatlas1/manzanil/Documents/LEGEND/PEN/AttenuationPenSetup/build/CMakeFiles/PEN.dir/src/PenMaterials.cc.o"
+  "/home/iwsatlas1/manzanil/Documents/LEGEND/PEN/AttenuationPenSetup/src/PhysicsList.cc" "/home/iwsatlas1/manzanil/Documents/LEGEND/PEN/AttenuationPenSetup/build/CMakeFiles/PEN.dir/src/PhysicsList.cc.o"
+  "/home/iwsatlas1/manzanil/Documents/LEGEND/PEN/AttenuationPenSetup/src/PrimaryGeneratorAction.cc" "/home/iwsatlas1/manzanil/Documents/LEGEND/PEN/AttenuationPenSetup/build/CMakeFiles/PEN.dir/src/PrimaryGeneratorAction.cc.o"
+  "/home/iwsatlas1/manzanil/Documents/LEGEND/PEN/AttenuationPenSetup/src/PrimaryGeneratorMessenger.cc" "/home/iwsatlas1/manzanil/Documents/LEGEND/PEN/AttenuationPenSetup/build/CMakeFiles/PEN.dir/src/PrimaryGeneratorMessenger.cc.o"
+  "/home/iwsatlas1/manzanil/Documents/LEGEND/PEN/AttenuationPenSetup/src/RunAction.cc" "/home/iwsatlas1/manzanil/Documents/LEGEND/PEN/AttenuationPenSetup/build/CMakeFiles/PEN.dir/src/RunAction.cc.o"
+  "/home/iwsatlas1/manzanil/Documents/LEGEND/PEN/AttenuationPenSetup/src/SiliconPlateConstruction.cc" "/home/iwsatlas1/manzanil/Documents/LEGEND/PEN/AttenuationPenSetup/build/CMakeFiles/PEN.dir/src/SiliconPlateConstruction.cc.o"
+  "/home/iwsatlas1/manzanil/Documents/LEGEND/PEN/AttenuationPenSetup/src/SteppingAction.cc" "/home/iwsatlas1/manzanil/Documents/LEGEND/PEN/AttenuationPenSetup/build/CMakeFiles/PEN.dir/src/SteppingAction.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "G4INTY_USE_QT"
+  "G4INTY_USE_XT"
+  "G4LIB_BUILD_DLL"
+  "G4MULTITHREADED"
+  "G4UI_USE"
+  "G4UI_USE_QT"
+  "G4UI_USE_TCSH"
+  "G4UI_USE_XM"
+  "G4VERBOSE"
+  "G4VIS_USE"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLQT"
+  "G4VIS_USE_OPENGLX"
+  "G4VIS_USE_OPENGLXM"
+  "G4VIS_USE_RAYTRACERX"
+  "G4_STORE_TRAJECTORY"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/opt/clhep/lib/CLHEP-2.4.1.0/../../include"
+  "../include"
+  "/opt/geant4/include/Geant4"
+  "/usr/include/QtCore"
+  "/usr/include/QtGui"
+  "/usr/include/QtOpenGL"
+  "/opt/clhep/include"
+  "/opt/hdf5/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/PEN.dir/cmake_clean.cmake
@@ -0,0 +1,22 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PEN.dir/PEN.cc.o"
+  "CMakeFiles/PEN.dir/src/ActionInitialization.cc.o"
+  "CMakeFiles/PEN.dir/src/DetectorConstruction.cc.o"
+  "CMakeFiles/PEN.dir/src/DetectorMessenger.cc.o"
+  "CMakeFiles/PEN.dir/src/EventAction.cc.o"
+  "CMakeFiles/PEN.dir/src/LightGuideConstruction.cc.o"
+  "CMakeFiles/PEN.dir/src/PenMaterials.cc.o"
+  "CMakeFiles/PEN.dir/src/PhysicsList.cc.o"
+  "CMakeFiles/PEN.dir/src/PrimaryGeneratorAction.cc.o"
+  "CMakeFiles/PEN.dir/src/PrimaryGeneratorMessenger.cc.o"
+  "CMakeFiles/PEN.dir/src/RunAction.cc.o"
+  "CMakeFiles/PEN.dir/src/SiliconPlateConstruction.cc.o"
+  "CMakeFiles/PEN.dir/src/SteppingAction.cc.o"
+  "PEN"
+  "PEN.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PEN.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
